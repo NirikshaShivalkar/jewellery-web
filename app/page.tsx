887764fa9a529f1510528dc60ac4c253f8a179eb
NirikshaShivalkar <nirikshashivalkar@gmail.com>
2026-05-21 18:32:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ export default function Home() {
         {/* IMAGE WITH SLOW ZOOM (this is what makes it premium) */}
         <img
           src={item.img}
-          className={`w-full h-full md:object-cover object-[center_30%]" ${
+          className={`className="w-full h-full object-cover object-center md:object-cover scale-[1.05]" ${
             i === slide ? "animate-[zoom_8s_linear]" : ""
           }`}
         />
